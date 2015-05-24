656997186d9da328a3cea878d5d957e263294637
Liz Honing <liz.honing@salespreso.com>
2015-05-24 20:38:35 +1000

    feat(things): all the things

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var banner = require("./lib/banner");
 
 function init() {
 	console.log(banner);
-	var server = require("./lib/server");	
+	var server = require("./lib/server");
 }
 
 module.exports = init();
